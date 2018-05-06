60b6d8ff916c2cbac7849a9a7c55e83a1901b432
Jason Chen <shchenchang@gmail.com>
2018-05-06 18:31:39 +0800

    1160 Output Limit Exceed

--- 1160/main.cpp
+++ 1160/main.cpp
@@ -25,11 +25,11 @@ void f(int plate, int apple, int* plates, int n, int * c)
 		if (plate == n-1 && apple == a)
 		{
 			(*c)++;
-			//for (int i = 0; i < n; i++)
-			//{
-			//	printf("%d ", plates[i]);
-			//}
-			//printf("\n");
+			for (int i = 0; i < n; i++)
+			{
+				printf("%d ", plates[i]);
+			}
+			printf("\n");
 			return;
 		}
 		f(plate + 1, apple - a, plates, n, c);
@@ -41,8 +41,12 @@ int main()
     int m, n;
 	int plates[10];
 	int c;
-    while (scanf("%d %d", &m, &n) != EOF)
+	int count;
+	scanf("%d", &count);
+
+    while (count--)
     {
+		scanf("%d %d", &m, &n);
 		c = 0;
 		f(0, m, plates, n, &c);
 		printf("%d\n", c);
