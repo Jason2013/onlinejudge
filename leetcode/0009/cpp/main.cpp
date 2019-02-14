468cc3493b7daa2cb28bc471760043774ae3c154
Jason Chen <shchenchang@gmail.com>
2019-02-14 15:17:41 +0800

    Pass user test cases.

--- leetcode/0009/cpp/main.cpp
+++ leetcode/0009/cpp/main.cpp
@@ -1,18 +1,57 @@
 #include <cassert>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    bool func(int x) {
+    bool isPalindrome(int x) {
+        if (x < 0)
+        {
+            return false;
+        }
 
+        if (x < 10)
+        {
+            return true;
+        }
+
+        vector<int> nums;
+
+        while (x != 0)
+        {
+            int r = x % 10;
+            nums.push_back(r);
+            x /= 10;
+        }
+
+        uint32_t first = 0;
+        uint32_t last = static_cast<uint32_t>(nums.size() - 1);
+        while (first < last)
+        {
+            if (nums[first] != nums[last])
+            {
+                return false;
+            }
+
+            ++first;
+            --last;
+        }
+
+        return true;
     }
 };
 
 int main()
 {
     Solution s;
-    assert(false);
+
+    assert(s.isPalindrome(121) == true);
+    assert(s.isPalindrome(-121) == false);
+    assert(s.isPalindrome(10) == false);
+    assert(s.isPalindrome(0) == true);
+    assert(s.isPalindrome(1) == true);
+    assert(s.isPalindrome(11) == true);
 
     return 0;
 }
