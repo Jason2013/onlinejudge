b5d3bd4cbc3772a9187153e6d2080d18521e4ea4
Jason Chen <shchenchang@gmail.com>
2019-02-14 13:15:17 +0800

    Add skeleton code.

--- leetcode/0007/cpp/main.cpp
+++ leetcode/0007/cpp/main.cpp
@@ -2,9 +2,20 @@
 #include <iostream>
 using namespace std;
 
+class Solution {
+public:
+    int reverse(int x) {
+
+    }
+};
+
 int main()
 {
-    assert(false);
+    Solution s;
+
+    //assert(s.reverse(123) == 321);
+    //assert(s.reverse(-123) == -321);
+    //assert(s.reverse(120) == 21);
 
     return 0;
 }
