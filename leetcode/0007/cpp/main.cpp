f631fc96856b3971f0f46e5ff44503f74c6ac346
Jason Chen <shchenchang@gmail.com>
2019-02-14 14:32:48 +0800

    Refine code.

--- leetcode/0007/cpp/main.cpp
+++ leetcode/0007/cpp/main.cpp
@@ -2,40 +2,34 @@
 #include <iostream>
 using namespace std;
 
+#define INTMIN (1 << 31)
+#define INTMAX (~INTMIN)
+
+#define INTMIN_LAST (INTMIN % 10)
+#define INTMAX_LAST (INTMAX % 10)
+
 class Solution {
 public:
     int reverse(int x) {
-        int flag = 1;
         int result = 0;
-        int remain = 0;
-
-        if (x < 0)
-        {
-            if (x == -2147483648)
-            {
-                // overflow
-                return 0;
-            }
-
-            x = -x;
-            flag = -1;
-        }
 
         while (x != 0)
         {
-            remain = x % 10;
+            int remain = x % 10;
             x /= 10;
 
-            if (result > 214748364 || (result == 214748364 && remain >= 8))
+            if (((result > INTMAX / 10) || (result == INTMAX / 10 && remain > INTMAX_LAST)) ||
+                ((result < INTMIN / 10) || (result == INTMIN / 10 && remain < INTMIN_LAST)))
             {
                 // overflow
                 return 0;
             }
+
 
             result = result * 10 + remain;
         }
 
-        return result * flag;
+        return result;
     }
 };
 
@@ -47,7 +41,7 @@ int main()
     assert(s.reverse(-123) == -321);
     assert(s.reverse(120) == 21);
     assert(s.reverse(1534236469) == 0);
-    assert(s.reverse(-2147483648) == 0);
+    assert(s.reverse(INTMIN) == 0);
 
     return 0;
 }
