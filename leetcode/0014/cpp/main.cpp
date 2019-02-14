49c5ed4979cae0a670d8cf6208746e3ff8617c13
Jason Chen <shchenchang@gmail.com>
2019-02-14 15:40:06 +0800

    Pass user test cases.

--- leetcode/0014/cpp/main.cpp
+++ leetcode/0014/cpp/main.cpp
@@ -1,18 +1,41 @@
 #include <cassert>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    bool func(int x) {
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix;
 
+        uint32_t pos = 0;
+        while (pos <= strs[0].size() - 1)
+        {
+            char c = strs[0][pos];
+            for (uint32_t si = 1; si < static_cast<uint32_t>(strs.size()); si++)
+            {
+                if (pos > strs[si].size() - 1 || strs[si][pos] != c)
+                {
+                    return prefix;
+                }
+            }
+
+            prefix += c;
+            ++pos;
+        }
+
+        return prefix;
     }
 };
 
 int main()
 {
     Solution s;
-    assert(false);
+
+    assert(s.longestCommonPrefix(vector<string>{"flower", "flow", "flight"}) == "fl");
+    assert(s.longestCommonPrefix(vector<string>{"dog", "racecar", "car"}) == "");
+    assert(s.longestCommonPrefix(vector<string>{"dog", "racecar", ""}) == "");
+    assert(s.longestCommonPrefix(vector<string>{"", "racecar", "car"}) == "");
 
     return 0;
 }
