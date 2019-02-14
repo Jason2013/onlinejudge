b0580eaf1662105076611f4c7f40ab66d80c8b89
Jason Chen <shchenchang@gmail.com>
2019-02-14 15:05:33 +0800

    "Add problem [0009]."

---
+++ leetcode/0009/cpp/main.cpp
@@ -0,0 +1,18 @@
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool func(int x) {
+
+    }
+};
+
+int main()
+{
+    Solution s;
+    assert(false);
+
+    return 0;
+}
