5494ae29cc7840437994dd51f4590f979c029c4b
Jason Chen <shchenchang@gmail.com>
2019-02-14 13:56:06 +0800

    Fix overflow.

--- leetcode/0007/cpp/main.cpp
+++ leetcode/0007/cpp/main.cpp
@@ -5,7 +5,31 @@ using namespace std;
 class Solution {
 public:
     int reverse(int x) {
+        int flag = 1;
+        int result = 0;
+        int remain = 0;
 
+        if (x < 0)
+        {
+            x = -x;
+            flag = -1;
+        }
+
+        while (x != 0)
+        {
+            remain = x % 10;
+            x /= 10;
+
+            if (result > 214748364 || (result == 214748364 && remain >= 8))
+            {
+                // overflow
+                return 0;
+            }
+
+            result = result * 10 + remain;
+        }
+
+        return result * flag;
     }
 };
 
@@ -13,9 +37,10 @@ int main()
 {
     Solution s;
 
-    //assert(s.reverse(123) == 321);
-    //assert(s.reverse(-123) == -321);
-    //assert(s.reverse(120) == 21);
+    assert(s.reverse(123) == 321);
+    assert(s.reverse(-123) == -321);
+    assert(s.reverse(120) == 21);
+    assert(s.reverse(1534236469) == 0);
 
     return 0;
 }
