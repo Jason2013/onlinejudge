7d76904a63203ad5b8a11cd9784a312c48c04991
Chang Chen <Chang.Chen@amd.com>
2020-12-17 15:06:14 +0800

    [leetcode-0004] Add unit test

--- leetcode/0004/python/main.py
+++ leetcode/0004/python/main.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -5,3 +7,17 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+
+        return 2
+
+class TestSolution(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_upper(self):
+        res = self.solution.findMedianSortedArrays([1, 2], [3]) 
+        self.assertEqual(res, 2)
+
+if __name__ == '__main__':
+    unittest.main()
