598476fd4be789db96c571d7ac23f63ae0c193ab
Chang Chen <Chang.Chen@amd.com>
2020-12-17 15:21:36 +0800

    [leetcode-0004] Add implementation of O(nlogn) complexity.

--- leetcode/0004/python/main.py
+++ leetcode/0004/python/main.py
@@ -8,15 +8,48 @@ class Solution(object):
         :rtype: float
         """
 
-        return 2
+        nums = sorted(nums1 + nums2)
+        length = len(nums)
+        if length % 2 == 1: # Odd
+            return nums[int((length - 1)/2)]
+        else: # Even
+            right = int(length/2)
+            left = right - 1
+            return (nums[left] + nums[right])/2
 
 class TestSolution(unittest.TestCase):
 
     def setUp(self):
         self.solution = Solution()
 
-    def test_upper(self):
-        res = self.solution.findMedianSortedArrays([1, 2], [3]) 
+    def test_case1(self):
+        nums1 = [1, 3]
+        nums2 = [2]
+        res = self.solution.findMedianSortedArrays(nums1, nums2) 
+        self.assertEqual(res, 2)
+
+    def test_case2(self):
+        nums1 = [1, 2]
+        nums2 = [3, 4]
+        res = self.solution.findMedianSortedArrays(nums1, nums2) 
+        self.assertEqual(res, 2.5)
+
+    def test_case3(self):
+        nums1 = [0, 0]
+        nums2 = [0, 0]
+        res = self.solution.findMedianSortedArrays(nums1, nums2) 
+        self.assertEqual(res, 0)
+
+    def test_case4(self):
+        nums1 = []
+        nums2 = [1]
+        res = self.solution.findMedianSortedArrays(nums1, nums2) 
+        self.assertEqual(res, 1)
+
+    def test_case5(self):
+        nums1 = [2]
+        nums2 = []
+        res = self.solution.findMedianSortedArrays(nums1, nums2) 
         self.assertEqual(res, 2)
 
 if __name__ == '__main__':
