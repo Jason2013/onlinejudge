a6534ba548a0b449a5f4225ce364241ea21a301d
Chang Chen <Chang.Chen@amd.com>
2020-12-21 16:08:45 +0800

    Pass all unit test cases

--- leetcode/0004/python/main.py
+++ leetcode/0004/python/main.py
@@ -36,33 +36,104 @@ class Solution(object):
 #            half = int(length/2) - 1
 #            odd = False
 
-        x = int(half/2)
-        y = half - x # - 1
-        while x >= 0 and x <= half:
+        low = 0
+        high = half
+        #x = half/2 # number of nums1
+        #while x >= 0 and x <= half:
+        while True:
+            x = (low + high)/2
+            if x < 0 or x > high:
+                break
+            x = int(x)
+            y = half - x # number of nums2
+            #assert x + y == half
             if x > n:
-                x = int((x-1)/2) # decrease x
+                high = x - 1
+
+                #x = int((x-1)/2) # decrease x
                 continue
             if y > m:
-                x = int((x+1+half)/2) # increase x
+                low = x + 1
+                #x = int((x+1+half)/2) # increase x
                 continue
             x1 = x-1
             x2 = x
             y1 = y-1
             y2 = y
-            if x1 in range(n) and y2 in range(m):
+# 
+            #if x1 == n - 1:
+            if x == n:
+                pass
+                #if y == 0:
                 if nums1[x1] <= nums2[y2]:
                     return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
                 else:
-                    x = int((x-1)/2) # decrease x
-                    continue
-            if x2 in range(n) and y1 in range(m):
+                    high = x - 1
+                    #x = decrease(x)
+#                pass
+#                elif y < m:
+#                    if nums1[x1] <= nums2[x2]:
+#                        return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
+#                    else:
+#                        x = decrease(x)
+#                    pass
+#                    
+#                    pass
+#                else:
+#                    assert False # impossible
+            #elif x2 == 0:
+            elif x == 0:
                 if nums2[y1] <= nums1[x2]:
                     return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
                 else:
-                    x = int((x+1+half)/2) # increase x
-                    continue
+                    low = x + 1
+                    #x = increase(x, half)
+                # pass
+            else:
+                assert x > 0 and x < n
+                if y == m:
+                    if nums2[y1] <= nums1[x2]:
+                        return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
+                    else:
+                        low = x + 1
+                        #x = increase(x)
+                    pass
+                elif y == 0:
+                    if nums1[x1] <= nums2[y2]:
+                        return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
+                    else:
+                        #x = decrease(x)
+                        high = x - 1
+                    pass
+                else:
+                    if nums1[x1] <= nums2[y2]:
+                        if nums2[y1] <= nums1[x2]:
+                            return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
+                        else:
+                            #x = increase(x)
+                            low = x + 1
+                    else:
+                        assert nums1[x1] > nums2[y2]
+                        #x = decrease(x)
+                        high = x - 1
+                    # assert y > 0 and y < m
+                    # pass
+                # pass
 
-            assert False
+            # if x1 in range(n) and y2 in range(m):
+            #     if nums1[x1] <= nums2[y2]:
+            #         return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
+            #     else:
+            #         x = int((x-1)/2) # decrease x
+            #         continue
+            # if x2 in range(n) and y1 in range(m):
+            #     if nums2[y1] <= nums1[x2]:
+            #         return self.findResult(odd, nums1, n, x1, x2, nums2, m, y1, y2)
+            #     else:
+            #         x = int((x+1+half)/2) # increase x
+            #         continue
+
+            # assert False
 
 #                    if odd:
 #                        return (nums1[x2] if x2 in range(n) else 0)
@@ -105,7 +176,12 @@ class Solution(object):
         # nums = sorted(nums1 + nums2)
         # return findMedianSortedArray(nums)
 
+    def increase(val, end):#, val):
+        return (val + 1 + end)/2
+    def decrease(val):
+        return (val - 1)/2
     def findResult(self, odd, nums1, n, x1, x2, nums2, m, y1, y2):
+        assert x1 + y1 == int((n + m)/2) - 2
         print (odd, nums1, n, x1, x2, nums2, m, y1, y2)
         if odd:
             #assert not (x2 not in range(n) and y2 not in range(m))
@@ -147,6 +223,16 @@ class TestSolution(unittest.TestCase):
 
     def setUp(self):
         self.solution = Solution()
+
+    def test_findMedianSortedArray1(self):
+        nums = [1]
+        res = self.solution.findMedianSortedArray(nums)
+        self.assertEqual(res, 1)
+
+    def test_findMedianSortedArray2(self):
+        nums = [1, 2]
+        res = self.solution.findMedianSortedArray(nums)
+        self.assertEqual(res, 1.5)
 
     def test_case1(self):
         nums1 = [1, 3]
