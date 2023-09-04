43aa159d6f1fc3f31dfae1be506248ba7f8d6cc6
Jason Chen <shchenchang@gmail.com>
2023-09-04 17:51:28 +0800

    Implement python code.

--- hackerrank/1_Week_Prepare_Kit/Day_1/Plus_Minus/python3/main.py
+++ hackerrank/1_Week_Prepare_Kit/Day_1/Plus_Minus/python3/main.py
@@ -14,6 +14,22 @@ import sys
 
 def plusMinus(arr):
     # Write your code here
+    p = 0
+    n = 0
+    z = 0
+    for v in arr:
+        if v > 0:
+            p += 1
+        elif v < 0:
+            n += 1
+        else:
+            z += 1
+
+    s = p + n + z
+    print(round(p/s, 6))
+    print(round(n/s, 6))
+    print(round(z/s, 6))
+
 
 if __name__ == '__main__':
     n = int(input().strip())
---
+++ hackerrank/1_Week_Prepare_Kit/Day_1/Plus_Minus/python3/run.bat
@@ -0,0 +1,3 @@
+del /q main.out
+python main.py < ..\cpp11\main.in > main.out
+type main.out
