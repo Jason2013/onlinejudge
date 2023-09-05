cc7c1758829a563d9070fa3d1614319442b96705
Jason Chen <shchenchang@gmail.com>
2023-09-05 14:25:49 +0800

    Upate test script files.

--- hackerrank/1_Week_Prepare_Kit/Day_2/Lonely_Integer/python3/run.bat
+++ hackerrank/1_Week_Prepare_Kit/Day_2/Lonely_Integer/python3/run.bat
@@ -1,4 +1,16 @@
-del /q main.out
-set OUTPUT_PATH=main.out
-python main.py < ..\cpp11\main.in
-type main.out
+@echo off
+setlocal EnableDelayedExpansion
+
+for %%i in (..\cpp11\main*.in) do (
+    @echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    rem echo %%i %%~ni.out
+    @echo --- input: %%i ---
+    type %%i
+    if exist %%~ni.out del /q %%~ni.out
+    set OUTPUT_PATH=%%~ni.out
+    python main.py < %%i
+    @echo +++ output: %%~ni.out +++
+    type %%~ni.out
+)
+
+endlocal
