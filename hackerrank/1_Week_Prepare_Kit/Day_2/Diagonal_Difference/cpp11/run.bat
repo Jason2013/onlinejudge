55f1af99149712c32781375f8436ada245d4b8f9
Jason Chen <shchenchang@gmail.com>
2023-09-05 15:39:01 +0800

    Update 'Day 2 - Diagonal_Difference' test code.

--- hackerrank/1_Week_Prepare_Kit/Day_2/Diagonal_Difference/cpp11/run.bat
+++ hackerrank/1_Week_Prepare_Kit/Day_2/Diagonal_Difference/cpp11/run.bat
@@ -8,7 +8,7 @@ for %%i in (main*.in) do (
     echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     echo --- input: %%i ---
     type %%i
-    del /q %%~ni.out
+    if exist %%~ni.out del /q %%~ni.out
     set OUTPUT_PATH=%%~ni.out
     build\Debug\main.exe < %%i
     echo +++ output: %%~ni.out +++
