5cfcae857ea7bc0340a0bdaf4b5a51fd3ec830ae
Jason Chen <shchenchang@gmail.com>
2023-09-06 19:06:46 +0800

    Add 'Day 3 - Caesar_Cipher' skeleton code.

---
+++ hackerrank/1_Week_Prepare_Kit/Day_3/Caesar_Cipher/cpp11/main.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+string caesarCipher(string s, int k) {
+    return "password";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string s;
+    getline(cin, s);
+
+    string k_temp;
+    getline(cin, k_temp);
+
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    string result = caesarCipher(s, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](int c){ return !isspace(c); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](int c){ return !isspace(c); }).base(),
+        s.end()
+    );
+
+    return s;
+}
